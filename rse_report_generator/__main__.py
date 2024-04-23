49b3beaf38db796ba74b64bbd183e21999cdfb90
Alex Dewar <a.dewar@imperial.ac.uk>
2024-04-23 17:37:38 +0100

    Start work on listing PRs

--- rse_report_generator/__main__.py
+++ rse_report_generator/__main__.py
@@ -1,1 +1,18 @@
-"""The entry point for the myproject program."""
+"""The entry point for RSE report generator."""
+
+import asyncio
+import sys
+
+from .report import generate_report
+
+
+async def main() -> None:
+    """Main entry point for program."""
+    if len(sys.argv) != 2:
+        raise RuntimeError("Repo name must be provided")
+
+    await generate_report(sys.argv[1], sys.stdout)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
