55db6913689c037bf5d2db05947c9ebfdb9d862a
Alex Dewar <a.dewar@imperial.ac.uk>
2024-05-08 15:49:28 +0100

    Display merged PRs over specified period

--- rse_report_generator/__main__.py
+++ rse_report_generator/__main__.py
@@ -2,16 +2,29 @@
 
 import asyncio
 import sys
+from datetime import datetime
+
+from dateparser import parse
 
 from .report import generate_report
 
 
+def _get_date(date_str: str) -> datetime:
+    if parsed_date := parse(date_str):
+        return parsed_date.astimezone()
+
+    raise ValueError(f"{date_str} is not a valid date string")
+
+
 async def main() -> None:
     """Main entry point for program."""
-    if len(sys.argv) != 2:
-        raise RuntimeError("Repo name must be provided")
+    repo_name = sys.argv[1]
+    from_date = _get_date(sys.argv[2])
+    to_date = (
+        _get_date(sys.argv[3]) if len(sys.argv) >= 4 else datetime.now().astimezone()
+    )
 
-    await generate_report(sys.argv[1], sys.stdout)
+    await generate_report(repo_name, from_date, to_date, sys.stdout)
 
 
 if __name__ == "__main__":
--- rse_report_generator/report.py
+++ rse_report_generator/report.py
@@ -1,10 +1,16 @@
 """Module for generating report."""
 
+from datetime import datetime
 from typing import TextIO
 
-from githubkit import GitHub, UnauthAuthStrategy
+from githubkit import (
+    BaseAuthStrategy,
+    GitHub,
+    TokenAuthStrategy,
+    UnauthAuthStrategy,
+)
 
-github = GitHub(UnauthAuthStrategy())
+from .gh_cli import get_github_token
 
 
 def get_repo_from_name(name: str) -> tuple[str, str]:
@@ -16,10 +22,27 @@ def get_repo_from_name(name: str) -> tuple[str, str]:
     return owner, repo
 
 
-async def generate_report(repo_name: str, fd: TextIO) -> None:
+async def _get_auth_strategy() -> BaseAuthStrategy:
+    if token := await get_github_token():
+        return TokenAuthStrategy(token)
+
+    return UnauthAuthStrategy()
+
+
+async def generate_report(
+    repo_name: str, from_date: datetime, to_date: datetime, fd: TextIO
+) -> None:
     """Generate a report and write to specified file."""
+    print(
+        f"# Report for {repo_name} ({from_date.date()} to {to_date.date()})\n", file=fd
+    )
+
+    github = GitHub(await _get_auth_strategy())
     owner, repo = get_repo_from_name(repo_name)
+
+    print("## Merged pull requests\n", file=fd)
     async for pr in github.paginate(
         github.rest.pulls.async_list, owner=owner, repo=repo, state="closed"
     ):
-        print(f"{pr.number}: {pr.title}", file=fd)
+        if pr.merged_at and from_date <= pr.merged_at < to_date:
+            print(f"- {pr.number}: {pr.title}", file=fd)
