c948a4b5b65678597efdc6c623530f9438e0dcbe
Alex Dewar <a.dewar@imperial.ac.uk>
2024-05-09 12:31:24 +0100

    Have single source of truth for program version

--- rse_report_generator/config.py
+++ rse_report_generator/config.py
@@ -1,10 +1,13 @@
 """Constants for use throughout the program."""
 
-from . import __name__ as PACKAGE_NAME  # noqa: F401
+from importlib.metadata import version
+
+from . import __name__ as PACKAGE_NAME
 
 APP_NAME = "RSE report generator"
 APP_DESCRIPTION = f"{APP_NAME}: A tool to automatically generate reports for projects"
 APP_DESCRIPTION_EPILOG = (
     'Dates can be given in a human-readable format (e.g. "one month ago").'
 )
+APP_VERSION = version(PACKAGE_NAME)
 PROG_NAME = "rse-report-generator"
